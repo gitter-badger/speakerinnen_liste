abb63f6e28631d0820902bd96b5b3c2ae19a7e20
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-10-20 19:28:38 +0200

    change static server assets

--- config/application.rb
+++ config/application.rb
@@ -50,7 +50,5 @@ module SpeakerinnenListe
     # Version of your assets, change this if you want to expire all your assets
     #config.assets.version = '1.0'
 
-    config.serve_static_assets = true
-
   end
 end
